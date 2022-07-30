c72a9747e56b67008e47e79b60549fd48cd69e01
Emerson Millard <emillard.code@gmail.com>
2022-07-30 12:00:00 -0700

    Added DTO class.

---
+++ src/main/java/com/project/dto/PaymentRequestDTO.java
@@ -0,0 +1,16 @@
+package com.project.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentRequestDTO {
+
+    private Integer orderId;
+    private Integer userId;
+    private Integer amount;
+
+}
