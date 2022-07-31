ca22595d661cf71567ec25ffe9ec521a53078f25
Emerson Millard <emillard.code@gmail.com>
2022-07-31 12:00:00 -0700

    Updated event class.

--- src/main/java/com/project/event/OrderEvent.java
+++ src/main/java/com/project/event/OrderEvent.java
@@ -1,4 +1,15 @@
 package com.project.event;
 
+import com.project.dto.OrderRequestDTO;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class OrderEvent {
+
+    private UUID eventId=UUID.randomUUID();
+    private Date eventDate=new Date();
+    private OrderRequestDTO orderRequestDto;
+    private OrderStatus orderStatus;
+
 }
