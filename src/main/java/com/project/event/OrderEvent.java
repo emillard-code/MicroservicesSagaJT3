06dd0639618f4b9fd7f4e0f5bc301cc80a4a3793
Emerson Millard <emillard.code@gmail.com>
2022-08-01 12:00:00 -0700

    Updated event class.

--- src/main/java/com/project/event/OrderEvent.java
+++ src/main/java/com/project/event/OrderEvent.java
@@ -5,11 +5,25 @@ import com.project.dto.OrderRequestDTO;
 import java.util.Date;
 import java.util.UUID;
 
-public class OrderEvent {
+public class OrderEvent implements Event {
 
     private UUID eventId = UUID.randomUUID();
     private Date eventDate = new Date();
     private OrderRequestDTO orderRequestDto;
     private OrderStatus orderStatus;
+
+    @Override
+    public UUID getEventId() {
+
+        return eventId;
+
+    }
+
+    @Override
+    public Date getDate() {
+
+        return eventDate;
+
+    }
 
 }
