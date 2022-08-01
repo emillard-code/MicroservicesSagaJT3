c874a48a8a31d5fab8cdf1c574afe88cf0a45c9b
Emerson Millard <emillard.code@gmail.com>
2022-08-01 12:00:00 -0700

    Updated event class.

--- src/main/java/com/project/event/PaymentEvent.java
+++ src/main/java/com/project/event/PaymentEvent.java
@@ -1,11 +1,29 @@
 package com.project.event;
 
+import com.project.dto.PaymentRequestDTO;
+
 import java.util.Date;
 import java.util.UUID;
 
-public class PaymentEvent {
+public class PaymentEvent implements Event {
 
     private UUID eventId=UUID.randomUUID();
     private Date eventDate = new Date();
+    private PaymentRequestDTO paymentRequestDto;
+    private PaymentStatus paymentStatus;
+
+    @Override
+    public UUID getEventId() {
+
+        return eventId;
+
+    }
+
+    @Override
+    public Date getDate() {
+
+        return eventDate;
+
+    }
 
 }
