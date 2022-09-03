95e2004bc4ce2a9bb837820b232a34224b4b0e82
Emerson Millard <emillard.code@gmail.com>
2022-09-03 15:26:31 -0700

    Initial commit

---
+++ src/main/java/com/project/microservicessagajt3/MicroservicesSagaJt3Application.java
@@ -0,0 +1,13 @@
+package com.project.microservicessagajt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesSagaJt3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesSagaJt3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/microservicessagajt3/MicroservicesSagaJt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservicessagajt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSagaJt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
